8a4be94d054db6a02447a16c5a3277a08520919a
Erika Vidal <diann852@gmail.com>
2017-03-06 18:25:33 -0500

    casi se muestra

---
+++ asssets/js/main.js
@@ -0,0 +1,12 @@
+var mostarOcultar = function(divtexte) {
+	var divtext = document.getElementById(divtexte);
+	if (divtext.style.display == "block" ) {
+		divtext.style.display = "none";
+	} else{
+		divtext.style.display = "block";
+	}
+
+}
+function mostrarResultados(){
+
+}
